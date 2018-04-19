c882ddd395054da2bd0f0ed126b3e6409e708cfe
Unknown <flemingcaleb@ksu.edu>
2018-04-18 23:49:24 -0500

    Begin implementation of web server in InfraBot.py
    Begin implementing the web server that will allow the applicaiton to receive event notificaitons from slack

--- InfraBot.py
+++ InfraBot.py
@@ -1,13 +1,29 @@
 import os				# To access tokens
 from DantesUpdater import danteUpdater	# To access DantesUpdator
+from flask import Flask
+from flask.ext.api import status
 
-dante = danteUpdater(os.environ['TESTING_TOKEN'])
+app = Flask(__name__)
 
-dante.start()
+@app.route("/")
+def main():
+	return "Welcome"
 
-input()
+@app.route("/test")
+def test():
+	print("RECIEVED TEST!")
+	return status.HTTP_200_OK
 
-dante.stop()
-print("Waiting for thread to stop")
-dante.join()
+#dante = danteUpdater(os.environ['TESTING_TOKEN'])
 
+#dante.start()
+
+#input()
+
+#dante.stop()
+#print("Waiting for thread to stop")
+#dante.join()
+
+
+if __name__ == "__main__":
+	app.run()

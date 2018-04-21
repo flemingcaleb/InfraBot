05d7615e6a108e0f046f4db534685354e803de38
Unknown <flemingcaleb@ksu.edu>
2018-04-21 10:40:42 -0500

    Update InfraBot to include basic modules for infra and user management

---
+++ InfraManager.py
@@ -0,0 +1,9 @@
+from InfraBot import InfraBot
+
+class InfraManager:
+    def __init__(self):
+        self.num = 0
+
+    def api_entry(self, message, channel, user):
+        InfraBot.sendMessage("Module not yet implemented")
+        return "Module not yet implemented"
---
+++ UserManager.py
@@ -0,0 +1,9 @@
+from InfraBot import InfraBot
+
+class UserManager:
+    def __init__(self):
+        self.num = 0
+
+    def api_entry(self, message, channel, user):
+        Infrabot.sendMessage("Module not yet implemented")
+        return "Module not yet implemented"

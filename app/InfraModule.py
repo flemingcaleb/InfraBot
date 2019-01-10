04e9c07be5a40b8925c2d0bc329a8c8987f1c1b8
Caleb Fleming <flemingcaleb@ksu.edu>
2019-01-10 10:20:36 +0000

    Comment out default message send in InfraModule

--- app/InfraModule.py
+++ app/InfraModule.py
@@ -4,7 +4,7 @@ class InfraModule:
         self.options = options
 
     def api_entry(self, message, channel, user, team_id):
-        InfraBot.sendMessage("Module not yet implemented", channel, team_id)
+        #InfraBot.sendMessage("Module not yet implemented", channel, team_id)
         return "Module not yet implemented"
 
     def action_entry(self, form_data):

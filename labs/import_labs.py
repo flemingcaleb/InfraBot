7b401a5c1fc709c5fcf825066856590d95c8a37d
Unknown <flemingcaleb@ksu.edu>
2019-01-12 14:09:19 -0600

    Begin work on adding to the DB from import_lab script

--- labs/import_labs.py
+++ labs/import_labs.py
@@ -1,11 +1,20 @@
 import glob   
 import json
 
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'app'))
+import Database
 
 path = './lab_contents/*.json'   
 files=glob.glob(path)   
 for file in files:     
     f=open(file, 'r')  
     curObj = json.load(f)   
-    print(curObj['name'])
+    newLab = Database.Labs()
+    newLab.name = curObj['name']
+    newLab.workspace_id = curObj['workspace_id']
+    newLab.url = curObj['url']
+    newLab.difficulty = curObj['url']
+    newLab.possible_score = curObj['max_score']
+    print(newLab.id)
     f.close()
